febaaca6a7f56daece0fb0695cc93c0dd87f83cb
Moeid Saleem Khan <moeidsaleem@gmail.com>
2022-11-15 01:20:11 +0400

    basic boilerplate setup :fire:

--- app/lib/agenda.ts
+++
@@ -1,7 +0,0 @@
-import Agenda from 'agenda';
-import config from '../config';
-
-export default () => {
-    return new Agenda();
-  
-  };
---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+function sayMyName(name) {
+    if (name === "Heisenberg") {
+        console.log("You're right 👍");
+    }
+    else {
+        console.log("You're wrong 👎");
+    }
+}
+sayMyName("Heisenberg");
